6bdd08969ca6d2cbe83299abb9c2da038023d816
Zhao Yue <yue.zhao@oaknorth.ai>
2020-06-01 11:11:23 +0800

    Adjust timeout values in websocketConnection

--- frontend/src/lib/WebsocketConnection.tsx
+++ frontend/src/lib/WebsocketConnection.tsx
@@ -44,13 +44,13 @@ const WEBSOCKET_STREAM_PATH = "stream"
 /**
  * Wait this long between pings, in millis.
  */
-const PING_RETRY_PERIOD_MS = 500
+const PING_RETRY_PERIOD_MS = 1000
 
 /**
  * Timeout when attempting to connect to a websocket, in millis.
  * This should be <= bootstrap.py#BROWSER_WAIT_TIMEOUT_SEC.
  */
-const WEBSOCKET_TIMEOUT_MS = 1000
+const WEBSOCKET_TIMEOUT_MS = 5000
 
 /**
  * If the ping retrieves a 403 status code a message will be displayed.
